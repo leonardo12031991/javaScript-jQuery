6dbc62bbec223e71c308125c35313997559c52c4
leonardo suarez <leonardofab19@hotmail.com>
2020-06-24 18:30:19 -0300

    algo de javaScript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+        mode: 'fade',
+        captions: true,
+        slideWidth: 600
+      });
+
+      alert("Hello! I am an alert box!!");
+
+
+})
